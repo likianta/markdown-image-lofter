25b6007d02470777b134c595e5f1b3174891b209
likianta <likianta@foxmail.com>
2022-06-13 00:28:27 +0800

    feat: replace `rich-click[typer]` with `argsense`

--- markdown_image_lofter/__main__.py
+++ markdown_image_lofter/__main__.py
@@ -4,35 +4,36 @@ python -m markdown_image_exchange --help
 import os.path
 
 import lk_logger
+from argsense import cli
 from lk_utils.filesniff import normpath
 from lk_utils.filesniff import relpath
-from rich_click import rich_click
-from rich_click import typer
 
-rich_click.USE_RICH_MARKUP = True
 lk_logger.setup(show_varnames=True)
 
 
-def cli(
-        filepath: str = typer.Argument(
-            ..., help='the markdown file path (ends with [cyan]".md"[/]).\n\n'
-                      '[dim]╰[/dim] either relative or absolute path is ok.',
-            exists=True, file_okay=True, dir_okay=False,
-        ),
-        dir_o: str = typer.Option(
-            None, help='output directory.\n\n'
-                       '[dim]╰[/dim] if not given, will use the same directory '
-                       'as the input file. the filename will be "~.export.md".',
-        ),
-        config: str = typer.Option(
-            None, help=f'the config file path (ends with [cyan]".yaml"[/]).\n\n'
-                       f'[dim]╰[/dim] if not given, the built-in config '
-                       f'([magenta]{relpath("../config.yaml")}[/]) will be used.'
-        )
-):
+@cli.cmd()
+def main(filepath: str, dir_o: str = None, config: str = None):
+    """
+    args:
+        filepath:
+            the markdown file path (ends with [cyan]".md"[/]).
+            [dim]╰[/dim] either relative or absolute path is ok.
+    
+    kwargs:
+        dir_o (-d):
+            output directory.
+            [dim]╰[/dim] if not given, will use the same directory as the -
+            input file. the filename will be "~.export.md".
+        config (-c):
+            the config file path (ends with [cyan]".yaml"[/]).
+            [dim]╰[/dim] if not given, the built-in config -
+            ([magenta]"./config.yaml"[/]) will be used.
+    """
     file_i = normpath(filepath, force_abspath=True)
     if config:
         config = normpath(config, force_abspath=True)
+    else:
+        config = relpath('../config.yaml')
     if dir_o:
         dir_o = normpath(dir_o, force_abspath=True)
     else:
@@ -45,4 +46,4 @@ def cli(
     main(file_i, file_o, config_path=config)
 
 
-typer.run(cli)
+cli.run(main)
